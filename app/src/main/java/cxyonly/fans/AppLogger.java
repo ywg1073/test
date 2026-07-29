2f53a372cf456e4d2411e1599aca37729fbe3fd6
fk <159020716+ywg1073@users.noreply.github.com>
2026-07-29 21:41:09 +0800

    Add app/src/main/java/cxyonly/fans/AppLogger.java

---
+++ app/src/main/java/cxyonly/fans/AppLogger.java
@@ -0,0 +1,15 @@
+package cxyonly.fans;
+
+import android.content.Context;
+
+public class AppLogger {
+    public static synchronized void init(Context context) {
+    }
+
+    public static synchronized void log(String category, String message) {
+    }
+
+    public static String getLogFilePath() {
+        return "";
+    }
+}
